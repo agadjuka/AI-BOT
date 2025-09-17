5231665adac4848c85786f0e54c24311219a53f7
Илья <ilya@example.com>
2025-09-17 17:09:45 +0800

    Паралельность

--- run_local.py
+++ run_local.py
@@ -56,7 +56,7 @@ def main():
     if not load_env_file():
         sys.exit(1)
     
-    # Проверяем обязательные перемеанные
+    # Проверяем обязательные перемеанные gjkyfz
     if not check_required_vars():
         sys.exit(1)
     
